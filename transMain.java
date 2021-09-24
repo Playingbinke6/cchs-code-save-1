c7947de3619e42b4c0ab518f8f00a72e37ed69af
Playingbinke6 <91207302+Playingbinke6@users.noreply.github.com>
2021-09-24 09:52:50 -0500

    Update transMain.java

--- transMain.java
+++ transMain.java
@@ -2,8 +2,20 @@ public class transMain
 {
    public static void main(String[] args)
    {
+      Scanner scanP = new Scanner();
+      Scanner scanT = new Scanner();
+      
       
       public static void finalOut()
+      {
+         for (int i=0; i<arrProd.length; i++)
+         {
+            
+         }
+         
+         //this should print out the final report for all items
+      }
+      public static void trans()
       {
          
       }
