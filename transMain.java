c81fae270bc409951e122f68df5aecbfa6955be4
Warezac <91270803+Warezac@users.noreply.github.com>
2021-09-28 09:12:52 -0500

    Update transMain.java

--- transMain.java
+++ transMain.java
@@ -1,5 +1,76 @@
 import java.util.*;
 
+public class Product
+{
+   private String ProductId;
+   private double Bought;
+   private double Sold;
+   private String Descr;
+
+   public Product(String pro, double buy, double sell, String Desc)
+    {
+        ProductId = pro;
+        Bought = buy;
+        Sold = sell;
+        Descr = Desc;
+    }
+
+    public String getID()
+    {
+        return ProductId;
+    }
+
+    public double getBuy()
+    {
+        return Bought;
+    }
+
+    public double getSell()
+    {
+        return Sold;
+    }
+
+    public String getDisc()
+    {
+        return Descr;
+    }
+
+
+
+}
+
+
+public class transMain
+{
+   public static void main(String[] args)
+   {
+      Scanner scanP = new Scanner();
+      Scanner scanT = new Scanner();
+      
+   }
+   public static void finalOut()
+      {
+         for (int i=0; i<arrProd.length; i++)
+         {
+            
+         }
+         
+         //this should print out the final report for all items
+      }
+   public static void trans(String input)
+      {
+         String[] arrIn = input.split("\\s");
+         for (int i=0; i<arrProd.length; i++)
+         {
+            String prodId = arrProd[i].getId();
+            if(prodId.equals(arrIn[1]))
+            {
+               
+            }
+         }
+         
+      }
+}
 
 public class transMain
 {
